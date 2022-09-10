ca184cce00b149704249ecf9e43bc50fa2e26661
danilovaalina <ialinerpromax@gmail.com>
2022-09-10 19:27:05 +0300

    В классе Restaurant создай статическую константу

--- Restaurant.java
+++ Restaurant.java
@@ -4,6 +4,8 @@ import com.javarush.task.task27.task2712.kitchen.Cook;
 import com.javarush.task.task27.task2712.kitchen.Waiter;
 
 public class Restaurant {
+    private static final int ORDER_CREATING_INTERVAL = 100;
+
     public static void main(String[] args) {
         Tablet tablet = new Tablet(1);
         Cook cook = new Cook("Amigo");
