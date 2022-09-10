b7ad2e31a2791118a12598c379681f62ae281e54
danilovaalina <ialinerpromax@gmail.com>
2022-09-10 21:03:28 +0300

    Фикс бага Баг: два повара готовят один и тот же заказ. Такой аутпут получился потому, что Observable информирует всех своих Observer-ов, т.е. планшет направляет свой заказ всем известным ему поварам. Ожидаемое поведение: планшет направляет свой заказ в очередь, свободный повар берет заказы из очереди. Реализация функционала: Планшет толкает свой заказ в Observer, который хранит в себе ссылку на очередь. Observer заносит заказ в очередь.

--- OrderManager.java
+++
@@ -1,8 +0,0 @@
-public class OrderManager implements Observer {
-    private LinkedBlockingQueue<Order> orderQueue = new LinkedBlockingQueue();
-
-    public void update(Observable tablet, Object arg) {
-        Order order = (Order) arg;
-        orderQueue.add(order);
-    }
-}
